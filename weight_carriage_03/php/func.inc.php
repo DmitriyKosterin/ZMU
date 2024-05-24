5b540d09070fb785df97e49fa0f77a4c85af4d1d
DmitriyKosterin <144521350+DmitriyKosterin@users.noreply.github.com>
2024-05-24 20:28:34 +0300

    Add files via upload

---
+++ weight_carriage_03/php/func.inc.php
@@ -0,0 +1,56 @@
+<?php
+function print_select_year($start_year,$count_year)
+{
+  $current_year=strftime("%Y");
+  for($i=0;$i<$count_year;$i++)
+  {
+    $y=$start_year+$i;
+    if($y==$current_year)
+    {
+      echo "<option value=\"$y\" selected>$y</option>\n";
+      break;
+    }
+    else
+      echo "<option value=\"$y\">$y</option>\n";
+  }
+}
+
+
+function print_select_month()
+{
+      $m=array('01'=>'¤нварь',
+               '02'=>'февраль',
+               '03'=>'март',
+               '04'=>'апрель',
+               '05'=>'май',
+               '06'=>'июнь',
+               '07'=>'июль',
+               '08'=>'август',
+               '09'=>'сент¤брь',
+               '10'=>'окт¤брь',
+               '11'=>'но¤брь',
+               '12'=>'декабрь');
+  $current_month=strftime("%m");
+  foreach($m as $month_num=>$month_name)
+  {
+    if($current_month==$month_num)
+      echo "<option value=\"$month_num;$month_name\" selected> $month_name </option>\n";
+    else
+      echo "<option value=\"$month_num;$month_name\"> $month_name </option>\n";
+  }
+}
+
+
+function print_select_day()
+{
+  $current_day=strftime("%d");
+  for($nday=1;$nday<=31;$nday++)
+  {
+      $day=strlen($nday)==1 ? '0'. $nday : $nday;
+      if($day==$current_day)
+        echo "<option value=\"$day\" selected>$nday</option>\n";
+      else
+        echo "<option value=\"$day\">$nday</option>\n";
+  }
+}
+
