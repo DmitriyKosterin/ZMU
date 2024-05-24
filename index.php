fc9e10c59456dd11a4ab53b31270e34c05a83e47
DmitriyKosterin <144521350+DmitriyKosterin@users.noreply.github.com>
2024-05-24 19:52:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<!doctype html public "-//w3c//dtd html 3.2 final//ru">
+<html>
+  <head>
+    <?="<title>Весы и дозаторы</title>\n";?> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="styles_divs.css" type="text/css" />
+    <link rel="stylesheet" href="styles_fonts.css" type="text/css" />
+    <link rel="stylesheet" href="scales/show_active.css" type="text/css" />
+    <script src="jquery/jquery.min.js"></script>
+
+    <style>
+		a {
+		font-family : Verdana, Arial;
+		text-decoration: none;
+		color: #0032A0;
+		}
+		
+		a:hover {
+		color: #00945D;
+		}
+		
+		.body_scales {
+        border-color: #ADACAF;
+        border-width: 1px;
+        border-style: solid;
+		margin-top: 20px;
+      }
+  
+      .title_scales {
+        background-color: #00945D;
+		border-color: #0c89b7;
+        color: white;
+        text-align : center;
+		padding-top: 10px;
+        padding-bottom: 10px;
+      }  
+
+      .text_scales  {
+        font-family : Verdana, Arial;
+        font-size : 18pt;
+		border-color: #0c89b7;
+        color : white;
+		text-align : center;
+      }
+  
+      .data_scales  {
+        font-family : Verdana, Arial;
+        font-size : 12pt;
+        color : white;
+		border-color: #0c89b7;
+        text-align : center;
+		line-height : 20pt;
+      }   
+    </style>
+  </head>   
+
+  <body bgcolor="#ffffff">
+    <div style=" position: relative;
+      margin: 0px auto;
+      width: 975px;">
+
+    <div class="body_scales">
+      <p class="title_scales">
+        <table border=0 width=100%>
+          <tr>
+            <td><p class="text_scales">Весы вагонные</p></td>
+          </tr>
+        </table>
+      </p>
+      <p class="data_scales">
+		<a href="weight_carriage_01/">Весы №1</a><br>
+        <a href="weight_carriage_02/">Весы №2</a><br>
+        <a href="weight_carriage_03/">Весы №3</a><br>
+        <a href="weight_carriage_04/">Весы №4</a><br>
+      </p>
+    </div> <!-- body_one_section -->
+    <div class="body_scales">
+      <p class="title_scales">
+        <table border=0 width=100%>
+          <tr>
+            <td align=left width=90%>
+              <p class="text_scales">Весы автомобильные</p>
+            </td>
+          </tr>
+        </table>
+      </p>
+      <p class="data_scales">
+		<a href="weight_auto_01/">Весы №5</a><br>
+        <a href="weight_auto_02/">Весы №6</a><br>
+      </p>
+    </div> <!-- body_scales -->
+    <div class="body_scales">
+      <p class="title_scales">
+        <table border=0 width=100%>
+          <tr>
+            <td align=left width=90%>
+              <p class="text_scales">Фасовка в мешки и МКР</p>
+            </td>
+          </tr>
+        </table>
+      </p>   
+      <p class="data_scales">
+		<a href="ffs_01/">Фасовка №1</a><span id="ffs_01">&#9829;</span><br>
+		<a href="ffs_02/">Фасовка №2</a><span id="ffs_02">&#9829;</span><br>
+		<a href="ffs_03/">Фасовка №3</a><span id="ffs_03">&#9829;</span><br>
+		<a href="ffs_04/">Фасовка №4</a><span id="ffs_04">&#9829;</span><br>
+        <a href="ffs_05/">Фасовка №5</a><span id="ffs_05">&#9829;</span><br>
+        <a href="ffs_06/">Фасовка №6</a><span id="ffs_06">&#9829;</span><br>
+        <a href="ffs_07/">Фасовка №7</a><span id="ffs_07">&#9829;</span><br>
+        <a href="ffs_08/">Фасовка №8</a><span id="ffs_08">&#9829;</span><br>
+      </p>
+    </div> <!-- body_scales -->
+    <br>        
+
+    <div class="body_scales">
+      <p class="title_scales">
+        <table border=0 width=100%>
+          <tr>
+            <td align=left width=90%>
+              <p class="text_scales">Весы лабораторные</p>
+            </td>
+          </tr>
+        </table>
+      </p>
+      <p class="data_scales">
+        <a href="weight_lab_01/">Весы №7</a><br>
+      </p>
+    </div> <!-- body_scales -->
+	
+	<div class="body_scales">
+      <p class="title_scales">
+        <table border=0 width=100%>
+          <tr>
+            <td align=left width=90%>
+              <p class="text_scales">Логистический стол</p>
+            </td>
+          </tr>
+        </table>
+      </p>
+      <p class="data_scales">
+        <a href="vm_logo_01/">Стол №1</a><br>
+      </p>
+    </div>
+
+    <div id="div_end_page">
+    </div>
+    <br>
+  </body> 
+</html>
