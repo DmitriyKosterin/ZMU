54610239558fb326ac185a81e327864426074613
DmitriyKosterin <144521350+DmitriyKosterin@users.noreply.github.com>
2024-05-24 20:06:00 +0300

    Add files via upload

---
+++ weight_carriage_03/php/bag_report/bag_report_select.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=8" />
+    <title>Насыпь</title>
+    <link rel="stylesheet" href="../../css/pure-min.css">
+    <link rel="stylesheet" href="../../css/style.css">
+
+    <script>
+      function check_uncheck_All() {
+        isChecked=document.getElementById('all').checked
+        document.getElementById('smena1').checked=isChecked
+        document.getElementById('smena2').checked=isChecked
+        document.getElementById('smena3').checked=isChecked
+        document.getElementById('smena4').checked=isChecked
+      }
+    </script>
+
+  </head>
+  <body>
+    <script language="JavaScript">
+      document.oncontextmenu=new Function("return false;");
+    </script>
+
+    <div class="header">
+      <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">Отгрузка в мешках и биг-бегах в ж/д вагоны и автотранспорт</a>
+      </div>
+    </div>
+    <?php require("../func.inc.php"); ?>
+    <?php require("../config.inc.php"); ?>
+
+    <div class="content">  
+      <form class="pure-form" action="report.php" method="post">
+        <fieldset>
+          <legend>Начальная дата/время</legend>
+          <div class="pure-g">
+            <div class="pure-u-4-24">
+              <label for="year1">Год</label>
+              <select class="pure-u-23-24" name="year1"><?php print_select_year("2016",20); ?></select>
+            </div>
+            <div class="pure-u-4-24">
+              <label for="month1">Месяц</label>
+              <select class="pure-u-23-24" name="month1"><?php print_select_month(); ?></select>
+            </div>              
+            <div class="pure-u-4-24">
+              <label for="day1">День</label>
+              <select class="pure-u-23-24" name="day1"><?php print_select_day(); ?></select>
+            </div>
+            <div class="pure-u-4-24">
+              <label for="time1">Время</label>
+              <select class="pure-u-23-24" name="time1">
+                <option>00:00</option>
+                <option>08:00</option>
+                <option>20:00</option>
+                <option>24:00</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+      
+        <fieldset>
+          <legend>Конечная дата/время</legend>
+          <div class="pure-g">
+            <div class="pure-u-4-24">
+              <label for="year2">Год</label>
+              <select class="pure-u-23-24" name="year2"><?php print_select_year("2016",20); ?></select>
+            </div>
+            <div class="pure-u-4-24">
+              <label for="month2">Месяц</label>
+              <select class="pure-u-23-24" name="month2"><?php print_select_month(); ?></select>
+            </div>              
+            <div class="pure-u-4-24">
+              <label for="day2">День</label>
+              <select class="pure-u-23-24" name="day2"><?php print_select_day(); ?></select>
+            </div>
+            <div class="pure-u-4-24">
+              <label for="time2">Время</label>
+              <select class="pure-u-23-24" name="time2">
+                <option>00:00</option>
+                <option>08:00</option>
+                <option>20:00</option>
+                <option selected>24:00</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+      
+        <fieldset>
+          <legend>Смена</legend>
+          <div class="pure-g">
+            <div class="pure-u-2-24">
+              <label for="smena1" class="pure-checkbox">A</label>
+              <input type="checkbox" id="smena1" name="smena1">
+            </div>
+            <div class="pure-u-2-24">
+              <label for="smena2" class="pure-checkbox">Б</label>
+              <input type="checkbox" id="smena2" name="smena2">
+            </div>
+            <div class="pure-u-2-24">
+              <label for="smena3" class="pure-checkbox">В</label>
+              <input type="checkbox" id="smena3" name="smena3">
+            </div>
+            <div class="pure-u-2-24">
+              <label for="smena4" class="pure-checkbox">Г</label>
+              <input type="checkbox" id="smena4" name="smena4">
+            </div>
+            <div class="pure-u-2-24">
+              <label for="all" class="pure-checkbox">все смены</label>
+              <input type="checkbox" id="all" onClick="check_uncheck_All()">
+            </div>
+          </div>
+        </fieldset>
+      
+        <fieldset>
+          <input type="text" name="ntransport" maxlength=16 class="pure-input-1-4" placeholder="Номер вагона или автомобиля">
+        </fieldset>
+        <button type="submit" class="pure-button pure-button-primary">Выбор</button>
+        <button type="reset" class="pure-button">Сброс</button>
+      </form>
+    </div>
+  </body>
+</html>
---
+++ weight_carriage_03/php/bag_report/report.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=8" />
+    <title>Мешки и биг-беги</title>
+    <link rel="stylesheet" href="../../css/pure-min.css">
+    <link rel="stylesheet" href="../../css/style.css">
+  </head>
+  <body>
+    <?php require("../config.inc.php"); ?>
+
+    <script language="JavaScript">
+        document.oncontextmenu=new Function("return true;");
+    </script>
+
+    <?php
+      $smenas=[];
+      $str_smenas='';
+      $smenaselect='';
+      $year1=$_POST['year1'];
+      $month1= substr($_POST['month1'], 0, 2);
+      $day1=$_POST['day1'];
+      $time1=$_POST['time1'];
+      $year2=$_POST['year2'];
+      $month2= substr($_POST['month2'], 0, 2);
+      $day2=$_POST['day2'];
+      $time2=$_POST['time2'];
+      $ntransport=$_POST['ntransport'];
+      if(isset($_POST['smena1']))
+        $smenas[]=1;
+      if(isset($_POST['smena2']))
+        $smenas[]=2;
+      if(isset($_POST['smena3']))
+        $smenas[]=3;
+      if(isset($_POST['smena4']))
+        $smenas[]=4;
+
+      if(count($smenas)>0)
+      {
+        $smenaselect.='(';
+        foreach ($smenas as $smena) {
+          $smenaselect.="smena=$smena OR ";
+          $str_smenas.= ($smena==1)? 'A,': (($smena==2)? 'Б,':(($smena==3)? 'В,':(($smena==4)? 'Г,':'')));
+        }
+        $str_smenas=substr($str_smenas, 0, -1);
+        $smenaselect=substr($smenaselect, 0, -4);
+        $smenaselect.=') ';
+      }
+      else
+      {
+        echo "<p><h3>Не выбрано ни одной смены</h3></p>";
+        exit();
+      }
+      $datebegin="$year1" . ".$month1" . ".$day1";
+      $dateend="$year2" . ".$month2" . ".$day2";
+      $str_datebegin="$day1" . ".$month1" . ".$year1";
+      $str_dateend="$day2" . ".$month2" . ".$year2";
+      $timebegin=($time1=='24:00')? '23:59:59': $time1.':00';
+      $timeend=($time2=='24:00')? '23:59:59': $time2.':00';
+
+      $query="SELECT * FROM $input_manual_table  WHERE ";
+      if($ntransport!='')
+        $query.="ntransport='$ntransport' AND ";
+      $query.=$smenaselect . "AND (";
+      if($datebegin!=$dateend)
+      {
+        $query.="(wdate = '$datebegin' AND wtime >= '$timebegin') OR ";
+        $query.="(wdate = '$dateend' AND wtime <= '$timeend') OR ";
+        $query.="(wdate > '$datebegin' AND wdate <'$dateend')) ";
+      }
+      else
+        $query.="(wdate = '$datebegin' AND wtime >= '$timebegin' AND wtime <= '$timeend'))";
+      $part_qwery=substr($query,9);
+      $query.="ORDER BY wdate,wtime";
+      //echo $query;
+      $mysqli=mysqli_connect($MYSQL_BASE_HOST,$MYSQL_USER_NAME,$MYSQL_USER_PASS, $MYSQL_BASE_NAME);
+      if(mysqli_connect_errno()) {
+        echo("<h3>Ошибка соединения c базой данных</h3>");
+        exit();
+      }
+      $result=mysqli_query($mysqli,$query);
+      $result_count=mysqli_num_rows($result);
+      if($result_count>1000)
+      {
+          echo "<p><h3>Полученные данные содержат $result_count записей. Допускается делать выборки, содержащие не более 1000 записей.</h3></p>";
+          exit();
+      }
+      $cur_date_time=strftime("%d.%m.%Y / %H:%M:%S");
+    ?>
+    <div>
+      <h3>Отгрузка в мешках и биг-бегах в ж/д вагоны и автотранспорт</h3>
+      <h4>Дата/время формирования отчёта: <?= $cur_date_time ?></h4>
+      <h5>Начальная дата/время: <?= $str_datebegin . ' / ' .$time1 ?></h5>
+      <h5>Конечная дата/время:  <?= $str_dateend . ' / ' .$time2 ?></h5>
+      <h5>Смена: <?=$str_smenas?></h5>
+      <?php 
+        if(strlen($ntransport)>0)
+          echo "<h5>Номер: $ntransport</h5>";
+      ?>
+    </div>
+    <p>
+      <table class="tabledata">
+        <thead style="display:table-header-group">      
+          <tr>
+            <th>Дата</th>
+            <th>Время</th>
+            <th>№ вагона или<br>автотранспорта</th>
+            <th>Вес,т</th>
+            <th>Количество мешков<br>или биг-бегов</th>
+            <th>Продукт</th>
+            <th>Таб. №</th>
+            <th>Смена</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $ct=0;
+            while($row=mysqli_fetch_assoc($result))
+            {
+              $date=explode("-",$row['wdate']); //дата: 2017-10-30
+              $year=substr($date[0],2);         //последние 2 цифры года: 17
+              $wtime=$row['wtime'];
+              $ntransport=$row['ntransport'];
+              $sum_weight=sprintf("%.2f",$row['sum_weight']);
+              $nbags=$row['nbags'];
+              $product=$row['product'];
+              $tabn=$row['tabn'];
+              $smena_code=$row['smena'];
+              $smena_liter= ($smena_code==1)? 'A': (($smena_code==2)? 'Б':(($smena_code==3)? 'В':(($smena_code==4)? 'Г':'')));
+              $ct++;
+              echo  "<tr>".
+                      "<td>$date[2].$date[1].$year</td>".   //дата: 30.10.17
+                      "<td>$wtime</td>".                    //время
+                      "<td>$ntransport</td>".               //№ вагона или автотранспорта
+                      "<td>$sum_weight</td>".               //вес отгруженной продукции,т
+                      "<td>$nbags</td>".                    //количество мешков<br>или биг-бегов
+                      "<td>$product</td>".                  //продукт
+                      "<td>$tabn</td>".                     //таб. №
+                      "<td>$smena_liter".                   //литер смены
+                    "</tr>";
+            }
+            mysqli_free_result($result);
+          ?>
+        </tbody>        
+      </table>
+    </p>
+    <?php
+      $query_product="SELECT product,SUM(sum_weight) AS sum_weight,SUM(nbags) AS sum_nbags ". $part_qwery . " GROUP BY product";
+    ?>
+    <div class="txt-centered">
+      <p>Выбрано записей: <?=$ct ?></p><p>
+      <table class="tabledata">
+        <thead>
+          <tr><th>Продукт</th><th>Суммарный вес,т</th><th>Количество</th></tr>
+        </thead>
+        <tbody>
+          <?php
+            $result=mysqli_query($mysqli,$query_product);
+            while($row=mysqli_fetch_assoc($result))
+            {
+              echo  "<tr>" .
+                      "<td>{$row['product']}</td>" . 
+                      "<td>" . sprintf("%.2f",$row['sum_weight']) . "</td>" .
+                      "<td>" . $row['sum_nbags'] . "</td>" .  
+                    "</tr>";
+            };
+            mysqli_free_result($result);
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
